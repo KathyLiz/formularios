e158587c9fa3bbf6670b51d4d7642c31981d1ffc
Kathy Liz <kathyta45@hotmail.com>
2018-07-02 22:55:34 -0500

    Se añade singleton y funcionalidad para parseo de objeto formulario

--- DataConverter/src/main/java/com/example/util/Convertidor.java
+++ DataConverter/src/main/java/com/example/util/Convertidor.java
@@ -6,6 +6,7 @@
 package com.example.util;
 
 import com.example.entities.Formulario;
+import com.example.entities.FormularioData;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
@@ -14,13 +15,23 @@ import com.google.gson.GsonBuilder;
  * @author Kathy
  */
 public class Convertidor {
-    
+    private FormularioData frmData = FormularioData.getInstancia();
     public Convertidor(){}
     
-    public Formulario convertirJson (String data){
-        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+    public void convertirJson (String data){
+        Gson gson = new Gson();
+        System.out.println("DATA " + data);
         Formulario formulario = gson.fromJson(data, Formulario.class);
-        return formulario;
+        this.frmData.setFacultad(formulario.getFacultad());
+        this.frmData.setCarrera(formulario.getCarrera());
+        this.frmData.setNombres(formulario.getNombres());
+        this.frmData.setApellidos(formulario.getApellidos());
+        this.frmData.setDocumento(formulario.getDocumento());
+        this.frmData.setPeriodo(formulario.getPeriodo());
+        this.frmData.setFecha(formulario.getFecha());
+        this.frmData.setTipo(formulario.getTipo());
+        System.out.println("Objeto data "+  new Gson().toJson(this.frmData));
+       // return formulario;
     }
     
 }
